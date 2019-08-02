bea82a84cbd60df7c337be90e66dcee4ea882412
mc <43491231@qq.com>
2019-08-02 16:16:30 +0800

    first commit

---
+++ test13.java
@@ -0,0 +1,9 @@
+public class test14 {
+    public static void main(String[] args){
+	    int x = 3;
+        int y = x++ + ++x + x * 10;
+        System.out.print(x);
+        System.out.print();
+    }
+    
+}
