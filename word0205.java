7c446932570cdc32888f2c7b9f89d95f2e32d932
mc <43491231@qq.com>
2019-08-02 20:54:57 +0800

    first commit

---
+++ word0205.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class word0205 {
+public static void main(String[] args) {
+    // "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char c=(char)(int)(Math.random()*26+65);
+	System.out.println(c);
+    }
+}
