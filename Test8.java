2804f706ffaaf1c76056475bb08be6b6eae52f30
mc <43491231@qq.com>
2019-08-03 16:06:32 +0800

    first commit

---
+++ Test8.java
@@ -0,0 +1,20 @@
+
+public class Test8 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=9;i++) {
+            for(j=1;j<=i;j++) {
+            System.out.print(j+"*"+i+"="+(j*i)+"\t");
+        }
+    System.out.println();
+        }
+    }
+
+	
+	
+	
+	
+	
+	
+	
+}
