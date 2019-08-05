6412cf387a9179968b23493927c211ef54f85e1b
mc <43491231@qq.com>
2019-08-05 20:30:48 +0800

    first commit

---
+++ Test30.java
@@ -0,0 +1,100 @@
+import java.util.*;
+public class Test30{
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+  int i = r.nextInt(26);
+  char ch = (char) (i+'A');
+  System.out.println(ch);
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
