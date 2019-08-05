67978462aef5dee6220b73af568cd24c84be3524
mc <43491231@qq.com>
2019-08-05 18:44:44 +0800

    first commit

---
+++ Test8501.java
@@ -0,0 +1,13 @@
+public class Test8501{
+public static void main(String[] args) {
+    int a = 100;
+    int b = 200;
+    int tmp;
+    //进行数据交换
+    tmp = a;
+    a = b;
+    b = tmp;
+    System.out.println("交换后的a:"+ a + ",b:" + b);
+    }
+ 
+}
