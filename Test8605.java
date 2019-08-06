73a2862f411b99a5ee75f7eabf395db800712c19
made <43491231@qq.com>
2019-08-06 17:12:27 +0800

    first commit

---
+++ Test8605.java
@@ -0,0 +1,10 @@
+package com.mc.text;
+
+public class Test8605 {
+    public static void main(String[] args) {
+			long max = Long.MAX_VALUE;
+			long maxYears = max/1000/60/60/24/365+1970;
+			System.out.println(maxYears);
+		}
+	}
+
