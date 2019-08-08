cb9396d081b7e170322c282cf08afd175239fb95
made <43491231@qq.com>
2019-08-08 20:02:51 +0800

    first commit

---
+++ Test8802.java
@@ -0,0 +1,17 @@
+package com.mc.text;
+
+public class Test8802 {
+	    public static void main(String[] args) {
+	        //TODO自动生成的方法存根
+	        System.out.println("     ******");
+	        System.out.println("    *****  * ");
+	        System.out.println("   *****    *");
+	        System.out.println("  *****      *");
+	        System.out.println(" *************");
+	        System.out.println(" *           *");
+	        System.out.println(" *           *");
+	        System.out.println(" *           *");
+	        System.out.println(" *************");
+        }
+}
+
