27ba58452653ff25b6248232f75425647e5da2ff
made <43491231@qq.com>
2019-08-08 20:03:52 +0800

    first commit

---
+++ Test8803.java
@@ -0,0 +1,12 @@
+package com.mc.text;
+
+public class Test8803 {
+	 public static void main(String[] args)
+	    {
+	        //for循环，如果for后面(）内的条件一直成立，{}内的代码一直执行
+	        for(int i=50;i<=66;i++)
+	        {
+	            System.out.println("这时的 i 值为 "+i);
+	        }
+	    }
+}
