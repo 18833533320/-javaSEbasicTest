cb73ad6e5796c8f1b7c23a596fbeae5672fca6af
made <43491231@qq.com>
2019-08-08 20:04:34 +0800

    first commit

---
+++ Test8805.java
@@ -0,0 +1,13 @@
+package com.mc.text;
+
+public class Test8805 {
+	    public static void testFun(char n)
+	    {
+	        System.out.println(n);
+	    }
+	    public static void main(String[] args){
+	        testFun('B');
+    }
+}
+
+
