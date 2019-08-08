131bc91a9421bce4e8a8e84b9b680fb6172f3cec
made <43491231@qq.com>
2019-08-08 20:04:13 +0800

    first commit

---
+++ Test8804.java
@@ -0,0 +1,15 @@
+package com.mc.text;
+
+public class Test8804 {
+
+	    public static void main(String[] args){
+	        int a=7;
+	        if(5>3){
+	            int s=3;    //声明一个int类型的局部变量
+	            System.out.println("s="+s);
+	            System.out.println("a="+a);
+        }
+	    
+    }
+}
+
